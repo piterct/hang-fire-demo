73aa71224354cb947e093ca3c6c97abbe2f0d97b
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-10-25 22:43:51 -0300

    jobDelayed

--- HangFireDemo/HangFire.API/Controllers/ServicoController.cs
+++ HangFireDemo/HangFire.API/Controllers/ServicoController.cs
@@ -1,6 +1,7 @@
 ﻿using Hangfire;
 using HangFire.API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Security.Cryptography.X509Certificates;
 
 namespace HangFire.API.Controllers
@@ -18,6 +19,14 @@ namespace HangFire.API.Controllers
         public IActionResult ExecutaUmavez()
         {
             var jobFireForget = BackgroundJob.Enqueue(() => _notificacao.NotificaOk());
+
+            return Ok();
+        }
+
+        [HttpGet("rodarAposTempo")]
+        public IActionResult RodarAposTempo()
+        {
+            var jobDelayed = BackgroundJob.Schedule(() => _notificacao.NotificaOk(), TimeSpan.FromSeconds(10));
 
             return Ok();
         }
