c0318596faa000b6193439311be70aad5a8f6e98
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-10-25 22:14:43 -0300

    Initial Configuration HangFire

--- HangFireDemo/HangFire.API/Startup.cs
+++ HangFireDemo/HangFire.API/Startup.cs
@@ -1,3 +1,5 @@
+using Hangfire;
+using Hangfire.SqlServer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +28,22 @@ namespace HangFire.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            // Add Hangfire services.
+            services.AddHangfire(configuration => configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseSqlServerStorage(Configuration.GetConnectionString("HangfireConnection"), new SqlServerStorageOptions
+                {
+                    CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                    SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                    QueuePollInterval = TimeSpan.Zero,
+                    UseRecommendedIsolationLevel = true,
+                    DisableGlobalLocks = true
+                }));
+
+            // Add the processing server as IHostedService
+            services.AddHangfireServer();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +53,8 @@ namespace HangFire.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseHangfireDashboard();
 
             app.UseHttpsRedirection();
 
